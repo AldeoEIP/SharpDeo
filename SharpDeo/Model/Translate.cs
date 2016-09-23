a9256308bc808b29386f46aab4d7fcf0bedf3261
aloisdg <aloisdegouvello@live.fr>
2016-09-23 10:13:53 +0200

    feat(model): Add translation module

---
+++ SharpDeo/Model/Translate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpDeo.Model {
+    public class Translation {
+        public string TranslatedText { get; set; }
+        public string LangTo { get; set; }
+        public string LangFrom { get; set; }
+    }
+}
